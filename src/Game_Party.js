23059b0eafbf70798f5b811963a399cffa969946
Emilio Islas <exinyo@gmail.com>
2017-06-13 17:51:44 -0700

    Steps change
    Step only increases once per tile distance

---
+++ src/Game_Party.js
@@ -0,0 +1,12 @@
+//-----------------------------------------------------------------------------
+// Game_Party
+
+(function() {
+  Game_Party.prototype.steps = function() {
+    return Math.floor(this._steps);
+  };
+
+  Game_Party.prototype.increaseSteps = function() {
+    this._steps += $gamePlayer.moveTiles() / QMovement.tileSize;
+  };
+})();
