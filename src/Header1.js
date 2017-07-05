88d64a62881777575f58a30fe59bbf1255ab0a18
Emilio Islas <exinyo@gmail.com>
2017-07-05 16:43:50 -0700

    1.4.3

--- src/Header1.js
+++ src/Header1.js
@@ -9,13 +9,13 @@ if (!Imported.QPlus || !QPlus.versionCheck(Imported.QPlus, '1.4.0')) {
   throw new Error('Error: QMovement requires QPlus 1.4.0 or newer to work.');
 }
 
-Imported.QMovement = '1.4.2';
+Imported.QMovement = '1.4.3';
 
 //=============================================================================
  /*:
  * @plugindesc <QMovement>
  * More control over character movement
- * @author Quxios  | Version 1.4.2
+ * @author Quxios  | Version 1.4.3
  *
  * @repo https://github.com/quxios/QMovement
  *
