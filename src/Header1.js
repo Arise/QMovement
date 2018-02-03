689695b28c4c906c00938d1a95865b1116ebbeb3
Emilio Islas <exinyo@gmail.com>
2018-02-02 16:32:19 -0800

    QMovement 1.6.3

--- src/Header1.js
+++ src/Header1.js
@@ -9,14 +9,14 @@ if (!Imported.QPlus || !QPlus.versionCheck(Imported.QPlus, '1.6.0')) {
   throw new Error('Error: QMovement requires QPlus 1.6.0 or newer to work.');
 }
 
-Imported.QMovement = '1.6.2';
+Imported.QMovement = '1.6.3';
 
 //=============================================================================
 /*:
  * @plugindesc <QMovement>
  * More control over character movement
- * @version 1.6.2
- * @author Quxios  | Version 1.6.2
+ * @version 1.6.3
+ * @author Quxios  | Version 1.6.3
  * @site https://quxios.github.io/
  * @updateurl https://quxios.github.io/data/pluginsMin.json
  *
@@ -515,7 +515,7 @@ Imported.QMovement = '1.6.2';
  * ============================================================================
  * **Pathfind**
  * ----------------------------------------------------------------------------
- * https://quxios.github.io/#/plugins/QPathfind
+ * https://quxios.github.io/plugins/QPathfind
  *
  * QPathfind is an A* pathfinding algorithm. This algorithm can be pretty heavy
  * if you are doing pixel based movements. So avoid having to many pathfinders
@@ -536,7 +536,7 @@ Imported.QMovement = '1.6.2';
  * ----------------------------------------------------------------------------
  * **Collision Map**
  * ----------------------------------------------------------------------------
- * https://quxios.github.io/#/plugins/QM+CollisionMap
+ * https://quxios.github.io/plugins/QM+CollisionMap
  *
  * Collision Map is an addon for this plugin that lets you use images for
  * collisions. Note that collision map checks are a lot heavier then normal
@@ -546,7 +546,7 @@ Imported.QMovement = '1.6.2';
  * ----------------------------------------------------------------------------
  * **Region Colliders**
  * ----------------------------------------------------------------------------
- * https://quxios.github.io/#/plugins/QM+RegionColliders
+ * https://quxios.github.io/plugins/QM+RegionColliders
  *
  * Region Colliders is an addon for this plugin that lets you add colliders
  * to regions by creating a json file.
@@ -567,7 +567,7 @@ Imported.QMovement = '1.6.2';
  * ============================================================================
  * Formated Help:
  *
- *  https://quxios.github.io/#/plugins/QMovement
+ *  https://quxios.github.io/plugins/QMovement
  *
  * RPGMakerWebs:
  *
